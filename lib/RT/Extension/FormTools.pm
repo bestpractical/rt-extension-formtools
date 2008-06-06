79302ae3b8843d8c6d5a41d34233e8c3d5156b4b
Kevin Falcone <falcone@bestpractical.com>
2008-06-06 15:49:19 +0000

    * since we don't name fields CoreField-Subject (and doing so   would mean magic frobbing of request_args before Create)   share the meaning of what a CoreField is with a library function

--- lib/RT/Extension/FormTools.pm
+++ lib/RT/Extension/FormTools.pm
@@ -3,4 +3,22 @@ use strict;
 
 package RT::Extension::FormTools;
 
+our $VERSION = '0.03';
+
+=head2 is_core_field
+
+passed one argument (field name) and checks if
+that is a field that we consider 'core' to
+RT (subject, AdminCc, etc) rather than something
+which should be treated as a Custom Field.
+
+Naming a Custom Field Subject would cause
+serious pain with FormTools
+
+=cut
+
+sub is_core_field {
+   return $_[0] =~ /^(Requestors|Cc|AdminCc|Subject)$/;
+}
+
 1;
