f78f4141be1daebce8b69ff85f8f9b4e0a60e928
Chia-liang Kao <clkao@bestpractical.com>
2008-12-11 17:52:35 +0000

    add cf validation support.

--- lib/RT/Extension/FormTools.pm
+++ lib/RT/Extension/FormTools.pm
@@ -21,4 +21,48 @@ sub is_core_field {
    return $_[0] =~ /^(Requestors|Cc|AdminCc|Subject|UpdateContent|Attach)$/;
 }
 
+sub validate_cf {
+    my ($CF, $ARGSRef) = @_;
+    my $NamePrefix = "Object-RT::Ticket--CustomField-";
+    my $field = $NamePrefix . $CF->Id . "-Value";
+    my $valid = 1;
+    my $value;
+    my @res;
+    if ($ARGSRef->{"${field}s-Magic"} and exists $ARGSRef->{"${field}s"}) {
+        $value = $ARGSRef->{"${field}s"};
+        # We only validate Single Combos -- multis can never be user input
+        next if ref $value;
+    }
+    else {
+        $value = $ARGSRef->{$field};
+    }
+
+    my @values = ();
+    if ( ref $value eq 'ARRAY' ) {
+        @values = @$value;
+    } elsif ( $CF->Type =~ /text/i ) {
+        @values = ($value);
+    } else {
+        @values = split /\r*\n/, ( defined $value ? $value : '');
+    }
+    @values = grep $_ ne '',
+        map {
+            s/\r+\n/\n/g;
+            s/^\s+//;
+            s/\s+$//;
+            $_;
+        }
+        grep defined, @values;
+    @values = ('') unless @values;
+
+    foreach my $value( @values ) {
+        next if $CF->MatchPattern($value);
+
+        my $msg = "Input must match ". $CF->FriendlyPattern;
+        push @res, $msg;
+        $valid = 0;
+    }
+    return ($valid, @res);
+}
+
 1;
