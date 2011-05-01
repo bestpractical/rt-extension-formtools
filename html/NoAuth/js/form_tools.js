52ceb222f983bf9885a75231cdd0bc99b4d1c15a
Ruslan Zakirov <ruz@bestpractical.com>
2011-05-01 14:50:11 +0400

    support other inputs in enabled_by feature
    only checkboxes and radios were supported, now text, hidden
    and other inputs work

--- html/NoAuth/js/form_tools.js
+++ html/NoAuth/js/form_tools.js
@@ -13,9 +13,13 @@ function disable_form_field(disable, selector) {
 function should_disable_form_field( fields, values ) {
     for ( var i = 0; i<fields.length; i++ ) {
         var field = fields[i];
-        var active = jQuery('input:checked:enabled[name="'+ field +'"], input:checked:enabled[name="'+ field +'s"]').filter(function() {
+        var active = jQuery('input:enabled[name="'+ field +'"], input:enabled[name="'+ field +'s"]').filter(function() {
+            var value = this.value;
+            if ( this.type == 'radio' || this.type == 'checkbox' ) {
+                if ( jQuery(this).attr('checked') === undefined ) return 0;
+            }
             for ( var i = 0; i < values[field].length; i++ ) {
-                if ( this.value == values[field][i] ) { return 1 }
+                if ( value == values[field][i] ) { return 1 }
             }
             return 0;
         }).length;
