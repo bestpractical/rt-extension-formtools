d9016ec631b65f5e51eafb0a50547046604e5669
Jason Crome <jcrome@bestpractical.com>
2024-01-17 14:05:28 -0500

    Prep version 1.02

--- lib/RT/Extension/FormTools.pm
+++ lib/RT/Extension/FormTools.pm
@@ -3,7 +3,7 @@ use strict;
 
 package RT::Extension::FormTools;
 
-our $VERSION = '1.01';
+our $VERSION = '1.02';
 
 RT->AddStyleSheets('rt-extension-formtools.css');
 RT->AddJavaScript('rt-extension-formtools.js');
