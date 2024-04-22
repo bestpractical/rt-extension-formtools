62e51016108581de3c9ba8ab99b46786a10a4639
Jason Crome <jcrome@bestpractical.com>
2024-04-22 15:52:13 -0400

    Add to list of core fields supported by FomTools
    Of these new fields, only Owner can be added to a form. Even then, it
    doesn't allow for selection or autocompletion. By adding it to the list
    of core fields, it also fixes a bug where Owner gets added as a core
    field, but saved as a CustomField.
    
    Next up is to add Due and TimeEstimated to the form builder UI, and
    make these fields render with the proper format.

--- lib/RT/Extension/FormTools.pm
+++ lib/RT/Extension/FormTools.pm
@@ -278,6 +278,8 @@ Valid core fields include:
 
 =item AdminCc
 
+=item Owner
+
 =item Subject
 
 =item Content
@@ -285,6 +287,8 @@ Valid core fields include:
 =item Attach
 
 =item Due
+
+=item TimeEstimated
 
 =back
 
@@ -318,10 +322,12 @@ my %is_core_field = map { $_ => 1 } qw(
     Requestors
     Cc
     AdminCc
+    Owner
     Subject
     Content
     Attach
     Due
+    TimeEstimated
 );
 
 sub is_core_field {
