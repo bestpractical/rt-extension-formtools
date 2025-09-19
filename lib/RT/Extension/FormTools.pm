50fad42f2a08225889fed2c0b9e9174aaeee31e9
Craig Kaiser <craig@bestpractical.com>
2025-09-19 11:54:20 -0400

    Prepare 1.19

--- lib/RT/Extension/FormTools.pm
+++ lib/RT/Extension/FormTools.pm
@@ -3,7 +3,7 @@ use strict;
 
 package RT::Extension::FormTools;
 
-our $VERSION = '1.18';
+our $VERSION = '1.19';
 
 RT->AddStyleSheets('rt-extension-formtools.css');
 RT->AddJavaScript('rt-extension-formtools.js');
